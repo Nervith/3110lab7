73a5f2efc894c5e21304c630f8cb50f2a8371f2c
Nervith <nitinhero@hotmail.com>
2022-11-14 16:35:06 -0500

    try

--- Site.java
+++ Site.java
@@ -1,12 +1,11 @@
 import java.net.http.HttpResponse;
-
+//Nitin Alagu 101190100
 public class Site {
     public static final double TAX_RATE = 0.5;
 
     public static void main(String[] args) {
         ResidentialSite resSite = new ResidentialSite();
         Double residential = resSite.getBillableAmount();
-
 
         LifelineSite lifSite = new LifelineSite();
         Double life = lifSite.getBillableAmount();
